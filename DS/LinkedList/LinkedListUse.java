a2603801f78ac1fcf20d3b6bae29d833fb7904fe
Rohit Tank <rowhittank@gmail.com>
2022-06-23 19:11:24 +0530

    minor changes

--- DS/LinkedList/LinkedListUse.java
+++ DS/LinkedList/LinkedListUse.java
@@ -14,22 +14,25 @@ public class LinkedListUse extends LinkedList1{
         while(true){
             System.out.println("Enter option------------\n1.Display\n2.Find length\n3.Insert Node");
             System.out.println("4.Remove consec dups\n5.Delete Node\n6.Reverse LL\n7.Increment data of nodes.\n8.Exit------------");
-            switch (sc.nextInt()){
-                case 1: printNodes(list1); break;
-                case 2: System.out.println(len(list1)); break;
-                case 3: System.out.println("\nEnter node at index: ");
-                        list1 = insertNode(list1, sc.nextInt());
-                        break;
-                case 5: System.out.println("\nEnter node to be deleted: ");
-                        list1 = deleteNode(list1, sc.nextInt());
-                        break;
-                case 4: list1 = rmConsecutiveDuplicates(list1); break;
-                case 6: list1 = reverse(list1); break;
-                case 7: incrementNode(list1); break;
-                case 8: System.exit(0); break;
-                default:
+            switch (sc.nextInt()) {
+                case 1 -> printNodes(list1);
+                case 2 -> System.out.println(len(list1));
+                case 3 -> {
+                    System.out.println("\nEnter node at index: ");
+                    list1 = insertNode(list1, sc.nextInt());
+                }
+                case 4 -> list1 = rmConsecutiveDuplicates(list1);
+                case 5 -> {
+                    System.out.println("\nEnter node to be deleted: ");
+                    list1 = deleteNode(list1, sc.nextInt());
+                }
+                case 6 -> list1 = reverse(list1);
+                case 7 -> incrementNode(list1);
+                case 8 -> System.exit(0);
+                default -> {
                     System.out.println("Invalid option selected");
                     System.exit(0);
+                }
             }
             System.out.println();
         }
