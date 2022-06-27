5136872f55caa13b6e2730b406754f429233e997
Rohit Tank <rowhittank@gmail.com>
2022-06-27 09:04:35 +0530

    -

--- DS/LinkedList/LinkedListUse.java
+++ DS/LinkedList/LinkedListUse.java
@@ -13,7 +13,7 @@ public class LinkedListUse extends LinkedList1{
 
         while(true){
             System.out.println("Enter option------------\n1.Display\n2.Find length\n3.Insert Node");
-            System.out.println("4.Remove consec dups\n5.Delete Node\n6.Reverse LL\n7.Increment data of nodes.\n8.Exit------------");
+            System.out.println("4.Remove consec dups\n5.Delete Node\n6.Reverse LL\n7.Increment data of nodes.\n8.Find mid node\n9.Exit------------");
             switch (sc.nextInt()) {
                 case 1 -> printNodes(list1);
                 case 2 -> System.out.println(len(list1));
@@ -28,7 +28,8 @@ public class LinkedListUse extends LinkedList1{
                 }
                 case 6 -> list1 = reverse(list1);
                 case 7 -> incrementNode(list1);
-                case 8 -> System.exit(0);
+                case 8 -> System.out.println("Mid Node is: "+findMid(list1));
+                case 9 -> System.exit(0);
                 default -> {
                     System.out.println("Invalid option selected");
                     System.exit(0);
