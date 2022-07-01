1de6f94c428e4c2d81c1d4cb74b89e3a948d5439
Rohit Tank <rowhittank@gmail.com>
2022-07-01 11:57:00 +0530

    Stack Linked List implementation

--- DS/Stack/StackLinkedList.java
+++ DS/Stack/StackLinkedList.java
@@ -1,7 +1,44 @@
 package com.DSA.DS.Stack;
 
-public class StackLinkedList {
-    public static void main(String[] args) {
 
+class Node {
+    int data;
+    Node next;
+    public Node(int n){
+        this.data = n;
     }
 }
+
+
+public class StackLinkedList {
+    private Node head = null;
+    private int size = 0;
+    public StackLinkedList(){
+        head = null;
+        size = 0;
+    }
+    public void push(int n){
+        Node newNode = new Node(n);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+    public int pop() throws StackException{
+        if(head == null) throw new StackException();
+        int n = head.data;
+        head = head.next;
+        size--;
+        return n;
+    }
+    public boolean isEmpty(){
+        return head == null;
+    }
+    public int peek() throws StackException{
+        if(head == null) throw new StackException();
+        return head.data;
+    }
+    public int sizeLL(){
+        return size;
+    }
+}
+
