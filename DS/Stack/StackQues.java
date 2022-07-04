0455d9fa2586f22aaa3870763a2e0c90d354f188
Rohit Tank <rowhittank@gmail.com>
2022-07-04 22:03:25 +0530

    min reversals

--- DS/Stack/StackQues.java
+++ DS/Stack/StackQues.java
@@ -3,7 +3,7 @@ package com.DSA.DS.Stack;
 import java.util.Stack;
 
 public class StackQues {
-    static boolean balancedParanthesis(String str){
+    static boolean balancedParenthesis(String str){
         Stack<Character> st = new Stack<>();
 
         for(int i=0; i<str.length(); i++){
@@ -16,8 +16,33 @@ public class StackQues {
         }
         return st.isEmpty();
     }
+    static int minBracketReversal(String str){
+        if(str.length()%2 != 0) return -1;
+
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == '(') st.push('(');
+            else{
+                if(!st.isEmpty() && st.peek() == '(') st.pop();
+                else st.push(')');
+            }
+        }
+        int count = 0;
+        while(!st.isEmpty()){
+            char x1 = st.pop();
+            char x2 = st.pop();
+            if(x1 == x1) count++;
+            else count+=2;
+        }
+
+        return count;
+    }
     public static void main(String[] args) {
         String str = "(())";
-        System.out.println(balancedParanthesis(str));
+        System.out.println(balancedParenthesis(str));
+
+        str = ")((("; // => 3 | str="()()"=>0;| str="()(("=> 1
+
+        System.out.println(minBracketReversal(str));
     }
 }
