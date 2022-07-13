cf4d41a2fd16b3256cdb01fa4a7899088dc5643a
Rohit Tank <rowhittank@gmail.com>
2022-07-13 18:04:15 +0530

    queue main

--- DS/Queue/QueueMain.java
+++ DS/Queue/QueueMain.java
@@ -1,23 +1,12 @@
 package com.DSA.DS.Queue;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueMain {
     public static void main(String[] args) throws QueueException {
-        QueueLinkedList q1 = new QueueLinkedList();
-        q1.enqueue(34);
-        q1.enqueue(56);
-        q1.enqueue(44);
-        q1.enqueue(11);
-        q1.enqueue(9);
-        System.out.println(q1.size());
-        System.out.println(q1.front());
-        System.out.println(q1.dequeue());
-        System.out.println(q1.front());
-        System.out.println(q1.size());
-        System.out.println(q1.dequeue());
-        System.out.println(q1.dequeue());
-        q1.enqueue(23);
+
+
 
     }
 }
