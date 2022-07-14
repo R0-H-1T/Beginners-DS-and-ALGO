aea13687165b76e1538d26c72ee3d15359859b17
Rohit Tank <rowhittank@gmail.com>
2022-07-14 19:52:41 +0530

    count number of nodes in tree

--- DS/Tree/TreeInitial/TreePractice.java
+++ DS/Tree/TreeInitial/TreePractice.java
@@ -1,4 +1,10 @@
 package com.DSA.DS.Tree.TreeInitial;
 
 public class TreePractice {
+    public int numberOfNodes(TreeNode<Integer> root){
+        if(root == null) return 0;
+        int leftNodes = numberOfNodes(root.left);
+        int rightNodes = numberOfNodes(root.right);
+        return leftNodes + rightNodes + 1;
+    }
 }
