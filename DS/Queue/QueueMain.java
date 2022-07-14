4fda6d03e8a15633d957c00a97e6d5e20e57851d
Rohit Tank <rowhittank@gmail.com>
2022-07-14 10:55:12 +0530

    main

--- DS/Queue/QueueMain.java
+++ DS/Queue/QueueMain.java
@@ -5,8 +5,18 @@ import java.util.Queue;
 
 public class QueueMain {
     public static void main(String[] args) throws QueueException {
+       Queue<Integer> q1 = new LinkedList<>();
+       q1.add(34);
+       q1.add(5);
+       q1.add(89);
+       q1.add(99);
+        System.out.println(q1.toArray());
+        System.out.println(q1.peek());
 
-
+       QueuePractice pract = new QueuePractice();
+       pract.reverseQueue(q1);
+        System.out.println(q1.peek());
+        System.out.println(q1.toArray());
 
     }
 }
