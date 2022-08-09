71e3a1e6f15fd53ce7d6d5411fcd906640c71338
Rohit Tank <rowhittank@gmail.com>
2022-08-09 11:07:06 +0530

    public data and tree left, right

--- DS/Tree/TreeInitial/TreeNode.java
+++ DS/Tree/TreeInitial/TreeNode.java
@@ -1,8 +1,8 @@
 package com.DSA.DS.Tree.TreeInitial;
 
 public class TreeNode<T> {
-    T data;
-    TreeNode<T> left, right;
+    public T data;
+    public TreeNode<T> left, right;
     public TreeNode(T data){
         this.data = data;
     }
