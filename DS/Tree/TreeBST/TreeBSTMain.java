6274df59bcf2003e4a1306e0f01b6ccdc81148b3
Rohit Tank <rowhittank@gmail.com>
2022-08-09 11:05:53 +0530

    binary search tree traversal

--- DS/Tree/TreeBST/TreeBSTMain.java
+++ DS/Tree/TreeBST/TreeBSTMain.java
@@ -1,4 +1,18 @@
 package com.DSA.DS.Tree.TreeBST;
 
+import com.DSA.DS.Tree.TreeInitial.TreeMain;
+import com.DSA.DS.Tree.TreeInitial.TreeNode;
+
 public class TreeBSTMain {
+    public static boolean binarySearch(TreeNode<Integer> root, int n){
+        if(root == null) return false;
+        if(n == root.data) return true;
+        return (n < root.data) ? binarySearch(root.left, n) : binarySearch(root.right, n);
+    }
+    public static void main(String[] args){
+        TreeMain t = new TreeMain();
+        TreeNode<Integer> root = t.treeInput(true, 0, true);
+        t.printTreeNodes(root);
+        System.out.println(binarySearch(root, 4));
+    }
 }
