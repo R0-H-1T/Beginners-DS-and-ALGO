11cdd9b4dbd02d503c334bb1439dcf241282f14e
Rohit Tank <rowhittank@gmail.com>
2022-09-06 20:25:27 +0530

    priority queue

---
+++ DS/PriorityQueueHeaps/PQ_Basics/PriorityQueue.java
@@ -0,0 +1,15 @@
+package com.DSA.DS.PriorityQueueHeaps.PQ_Basics;
+
+//Priority Queue are of two types - Min, Max:
+//Min Ex:
+// 10 2  6  7  8        ~ are the priorities of the p1, p2, p3, p4, p5
+// p1 p2 p3 p4 p5
+//output - p2 p3 p4 p5 p1
+
+
+// many ways of implementing this, however one approach is :
+// using a heap: CBT(complete balanced tree), heap ordering property
+
+
+public class PriorityQueue {
+}
