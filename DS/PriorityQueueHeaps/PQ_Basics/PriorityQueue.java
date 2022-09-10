697a76f06fdfd941e73389d975e73b1a1da23cb7
Rohit Tank <rowhittank@gmail.com>
2022-09-10 22:10:49 +0530

    heap info and implementation of insert method

--- DS/PriorityQueueHeaps/PQ_Basics/PriorityQueue.java
+++ DS/PriorityQueueHeaps/PQ_Basics/PriorityQueue.java
@@ -8,8 +8,69 @@ package com.DSA.DS.PriorityQueueHeaps.PQ_Basics;
 
 
 // many ways of implementing this, however one approach is :
-// using a heap: CBT(complete balanced tree), heap ordering property
+// using a heap: CBT(complete binary tree), heap ordering property
 
+// in CBT, note that the elements  should start filling from the left.
+// if the tree has no element on its leftmost side, it is not considered to be a CBT
+
+//Using array to store in CBT
+// ith index (left child => 2*i + 1, right child => 2*i + 2) and to find the parent of the child use (i-1)/2
+
+//Types of heap
+//Min heap = its children should be greater than the root (at every level)
+//max heap = its children should be smaller than the root (at every level)
+
+import java.util.ArrayList;
 
 public class PriorityQueue {
+    //functions to implement =>  -------- [ FOR Min Heap]
+        // 1. isEmpty()
+        // 2. size()
+        // 3. getMin()
+        // 4. removeMin()
+        // 5. insert()
+    ArrayList<Element<String>> heap;
+    public PriorityQueue() {
+        heap = new ArrayList<>();
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+    public int size() {
+        return 0;
+    }
+    public void insert(String data, int priority) {
+        Element<String> x = new Element<String>(data, priority);
+        heap.add(x);
+        int childIndex = heap.size() - 1;
+        int parentIndex = (childIndex - 1)/2;
+        while (childIndex > 0){
+            if(heap.get(childIndex).priority < heap.get(parentIndex).priority) {
+                Element<String> temp = heap.get(parentIndex);
+                heap.set(parentIndex, heap.get(childIndex));
+                heap.set(childIndex, temp);
+            } else {
+                return;
+            }
+            childIndex = parentIndex;
+            parentIndex = (childIndex - 1)/2;
+        }
+    }
+    public String getMin() {
+        return "hello";
+    }
+    public String removeMin() {
+        return "hello";
+    }
+
+}
+
+//--------------------- data and its priority ---------------->
+class Element<T> {
+    T data;
+    int priority;
+    public Element(T data, int priority) {
+        this.data = data;
+        this.priority = priority;
+    }
 }
